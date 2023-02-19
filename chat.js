b7166e1e14f0f164449617518df1d07f6009984e
adityagothiwarekar <109978509+adityagothiwarekar@users.noreply.github.com>
2023-02-19 22:46:49 +0530

    chatbot changes

--- chat.js
+++ chat.js
@@ -94,7 +94,7 @@ function sendButton() {
 }
 
 function heartButton() {
-    buttonSendText("Heart clicked!")
+    buttonSendText("Welcome To Department Of EXTC DBIT!")
 }
 
 // Press enter to send a message
